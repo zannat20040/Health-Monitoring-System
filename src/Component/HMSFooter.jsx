f2b7fa61d5c812b7aec53966a72d604a251c3cee
zannat20040 <shanzidadisha1234@gmail.com>
2024-11-05 18:02:21 +0600

    footer design

--- src/Component/HMSFooter.jsx
+++ src/Component/HMSFooter.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function HMSFooter() {
   return (
-    <div>HMSFooter</div>
-  )
+    <footer className="footer mt-11 footer-center bg-gray-200 text-base-content p-4">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All rights reserved by ASH
+          Health Monitoring System. Developed by [Abid, Safayet, Hema]
+        </p>
+      </aside>
+    </footer>
+  );
 }
--- src/Users/UserLayout.jsx
+++ src/Users/UserLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HMSNavbar from "../Component/HMSNavbar";
 import { Outlet } from "react-router-dom";
+import HMSFooter from "../Component/HMSFooter";
 
 export default function UserLayout() {
   return (
     <div>
       <HMSNavbar />
       <Outlet />
+      <HMSFooter />
     </div>
   );
 }
